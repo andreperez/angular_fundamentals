19b3a0feb63b0521a3d0fdda58446a6559cf4a5c
André Eduardo Pérez Álvarez <andre.alvarez@totvs.com.br>
2018-07-24 16:30:59 -0300

    Service to hold our method for accessing the GitHub Search API. To share it alongside the application, we use dependency injection with the decorator @Injectable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from './git-search.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [GitSearchService]
 })
-export class AppComponent {
-  title = 'GitHub Browser';
+export class AppComponent implements OnInit {
+    title = 'app is functional!';
+
+    constructor(private gitSearchService: GitSearchService) {
+    }
+
+    ngOnInit() {
+        this.gitSearchService.gitSearch('angular').then((response) => {
+            alert('Total Libraries Found:' + response.total_count);
+        }, (error) => {
+            alert('Error: ' + error.statusText);
+        });
+    }
 }
