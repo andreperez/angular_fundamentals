b424995a976afeae4e6a163c6bf8d0a17a3c7d22
André Eduardo Pérez Álvarez <andre.alvarez@totvs.com.br>
2018-07-25 16:16:28 -0300

    Define Parameters and Redirects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,11 @@ const appRoutes: Routes = [
     },
     {
         path: 'search',
+        redirectTo: '/search/angular',
+        pathMatch: 'full'
+    },
+    {
+        path: 'search/:query',
         component: GitSearchComponent,
         data: {
             title: 'Git Search'
