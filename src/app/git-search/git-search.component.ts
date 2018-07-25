4124cb0cb74ee263649a19ec031d0058776ca46a
André Eduardo Pérez Álvarez <andre.alvarez@totvs.com.br>
2018-07-25 17:24:17 -0300

    To Use Route Parameters (agora a URL na parte superior do browser aparece conforme a rota usada)

--- src/app/git-search/git-search.component.ts
+++ src/app/git-search/git-search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, ParamMap, Router } from '@angular/router'
 
 import { GitSearchService } from "../git-search.service";
 import { GitSearch } from "../git-search";
@@ -14,20 +14,24 @@ export class GitSearchComponent implements OnInit {
     searchResults: GitSearch;
     searchQuery: string;
     title: string;
+    displayQuery: string;
 
-    constructor(private gitSearchService: GitSearchService, private route: ActivatedRoute) { }
+    constructor(
+        private gitSearchService: GitSearchService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) { }
 
     ngOnInit() {
-        this.gitSearchService.gitSearch('angular').then((response) => {
-            console.log(response);
-            this.searchResults = response;
-        }, (error) => {
-            alert("Error: " + error.statusText)
-        });
+        this.route.paramMap.subscribe( (params: ParamMap) => {
+            this.searchQuery = params.get('query');
+            this.displayQuery = params.get('query');
+            return this.gitSearch();
+        })
 
         this.route.data.subscribe(result => {
-            this.title = result.title;
             console.log(result);
+            this.title = result.title;
         });
     }
 
@@ -38,5 +42,10 @@ export class GitSearchComponent implements OnInit {
             alert("Error: " + error.statusText)
         })
     }
+
+    sendQuery = () => {
+        this.searchResults = null;
+        this.router.navigate(['/search/' + this.searchQuery]);
+      }
 
 }
